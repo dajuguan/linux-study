bd548beededd1d06b5e2c8f9094a7b4191010e2b
Po <chenjunfeng@ethstorage.io>
2023-12-25 04:02:47 +0100

    chap04: summary

--- 04-ipc/exercise_2/pipe.c
+++ 04-ipc/exercise_2/pipe.c
@@ -12,10 +12,16 @@ int main() {
 
     pipe(pair);
     write(pair[0], &buf, len);
+    buf[0] = '0';
+    write(pair[0], &buf, len);
     read(pair[1], &buf, len);
-    printf("rw success: %s", buf);
+    printf("rw success 0: %s", buf);
+
+    read(pair[0], &buf, len);  // blocked, because write(pair[0]) can only be read on pair[1]
+    printf("rw success 1: %s", buf);
+    
     buf[0] = '2';
     write(pair[1], &buf, len);
     read(pair[0], &buf, len);
-    printf("rw success: %s", buf);
+    printf("rw success 2: %s", buf);
 }
--- 2-file-system/fd_share.c
+++ 2-file-system/fd_share.c
@@ -4,27 +4,47 @@
 #include<unistd.h>
 #include <fcntl.h>
 
+#define MAX_LINE 100
 // 父子进程共享所有的file descriptor
 main(){
     char str[4]="asd";
+    int num_read;
+    char buf[MAX_LINE];
     pid_t pid=fork();
+    int PARENT_IN_FD = dup(STDIN_FILENO);
     if(pid==0){
         str[0]='b';
         printf("子进程中str=%s\n",str);
         write(STDIN_FILENO, &"aaa\n", 4);
         printf("子进程中str指向的首地址:%x\n",(unsigned int)str);
 
-
-        pid_t bc_pid;
-        char *const spawn_argv[] = {"bc" , NULL};
-        int ret = posix_spawnp(&bc_pid, "bc", NULL, NULL, spawn_argv, NULL);
-        if (ret != 0 ) {
-            printf("spawn bc program error: %u\n", ret);
-            exit(-1);
+        
+        while(num_read = read(PARENT_IN_FD, &buf, MAX_LINE)) {
+            printf("child read: %s", buf);  // when parent read, this will not be able to read
+            //  each file descriptor in the child refers to the
+            //   same open file description (see open(2)) as the corresponding
+            //   file descriptor in the parent.  This means that the two file
+            //   descriptors share open file status flags, file offset, and
+            //   signal-driven I/O attributes
         }
+
+        // pid_t bc_pid;
+        // char *const spawn_argv[] = {"bc" , NULL};
+        // int ret = posix_spawnp(&bc_pid, "bc", NULL, NULL, spawn_argv, NULL);
+        // if (ret != 0 ) {
+        //     printf("spawn bc program error: %u\n", ret);
+        //     exit(-1);
+        // }
     }
     else{
-        sleep(1);
+        close(STDIN_FILENO);  // 只是关闭了fd,不是stream
+        while(num_read = read(STDIN_FILENO, &buf, MAX_LINE)) {
+            if (num_read == -1) {
+                perror("read wrong\n");
+                break;
+            }
+            printf("parent read: %s", buf);
+        }
         printf("父进程中str=%s\n",str);
         printf("父进程中str指向的首地址:%x\n",(unsigned int)str);
     }
